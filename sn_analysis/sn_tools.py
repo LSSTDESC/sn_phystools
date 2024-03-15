05b4c9756cb56eff1f5264765b1ab9f2969a39ab
pgris <gris@clermont.in2p3.fr>
2024-03-15 18:23:16 +0100

    code protection

--- sn_analysis/sn_tools.py
+++ sn_analysis/sn_tools.py
@@ -594,7 +594,8 @@ def load_cosmo_data(theDir, dbName, cols_group, spectro_config,
 
     for fi in fis:
         dd = pd.read_hdf(fi)
-        dd['nsn_rat_highz'] = dd['nsn_z_0.8_sigma_mu'] / dd['nsn_z_0.8']
+        if 'nsn_z_0.8_sigma_mu' in dd.columns:
+            dd['nsn_rat_highz'] = dd['nsn_z_0.8_sigma_mu'] / dd['nsn_z_0.8']
         df = pd.concat((df, dd))
 
     dictagg = {}
