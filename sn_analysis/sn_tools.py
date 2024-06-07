9a5217cea0bc0c2e830ec12588fa23ca691bd18b
pgris <gris@clermont.in2p3.fr>
2024-06-07 17:06:26 +0200

    update to account for new cosmofit file names

--- sn_analysis/sn_tools.py
+++ sn_analysis/sn_tools.py
@@ -587,8 +587,10 @@ def load_cosmo_data(theDir, dbName, cols_group, spectro_config,
 
     """
 
-    fName = '{}/cosmo_{}.hdf5'.format(theDir, dbName)
+    fName = '{}/cosmo_{}*.hdf5'.format(theDir, dbName)
     fis = glob.glob(fName)
+    if len(fis) == 0:
+        print('Problem here: file not foun in path', fName)
 
     df = pd.DataFrame()
 
