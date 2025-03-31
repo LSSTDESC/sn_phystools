dfd9434414aa0cf1d27b5ea73df4afb3aec4d5ff
pgris <gris@clermont.in2p3.fr>
2025-03-31 10:53:53 +0200

    typo corr

--- sn_analysis/sn_selection.py
+++ sn_analysis/sn_selection.py
@@ -43,6 +43,7 @@ def selection_criteria():
     # dict_sel['G10_sigmaC_z0.7'] = dict_sel['G10_sigmaC'] + sdict['z0.7']
 
     dict_sel['G10_JLA'] = sdict['phases']+sdict['G10']+sdict['JLA']
+
     dict_sel['G10_JLA_sigmaC'] = sdict['phases'] + \
         sdict['G10']+sdict['JLA']+sdict['sigmaC']
 
