7ae7cd0e301326a42515c0a8dcdfe30fbbc90dd8
pgris <gris@clermont.in2p3.fr>
2025-08-25 11:19:49 +0200

    add common scripts

--- sn_cosmology/cosmo_tools.py
+++ sn_cosmology/cosmo_tools.py
@@ -5,6 +5,7 @@ Created on Thu Mar 21 12:46:30 2024
 
 @author: philippe.gris@clermont.in2p3.fr
 """
+import pandas as pd
 
 
 def get_surveys(name, data):
@@ -106,3 +107,85 @@ def get_survey_nickname(tagsurvey, surveys, data):
         surveys = get_surveys(tagsurvey, data)
 
     return tagsurvey, surveys
+
+
+def host_effi_1D(lista, listb):
+    """
+    Function to build a dict of 1D interpolators
+
+    Parameters
+    ----------
+    lista : list(str)
+        List of csv files with z,effi as columns.
+    listb : list(str)
+        List of keys for the output dict.
+
+    Returns
+    -------
+    dict_out : dict
+        Output data.
+
+    """
+
+    from scipy.interpolate import interp1d
+    dict_out = {}
+    for i, vv in enumerate(lista):
+        dd = pd.read_csv(vv, comment='#')
+        nn = listb[i]
+        dict_out[nn] = interp1d(dd['z'], dd['effi'],
+                                bounds_error=False, fill_value=0.)
+
+    return dict_out
+
+
+def load_host_effi(dataDir, llist):
+    """
+    Function to load all effi(z) csv files in a dict
+
+    Parameters
+    ----------
+    dataDir : str
+        Data dir.
+
+    Returns
+    -------
+    dictout : dict
+        key=name; val=interp1d(z,effi).
+
+    """
+
+    dictout = {}
+    for ll in llist:
+        fName = '{}/{}.csv'.format(dataDir, ll)
+        rr = host_effi_1D([fName], [ll])
+        dictout.update(rr)
+
+    return dictout
+
+
+def load_footprints(dataDir):
+    """
+    Function to load footprints
+
+    Parameters
+    ----------
+    dataDir : str
+        Location dir of the footprint files.
+
+    Returns
+    -------
+    df : pd.Dataframe
+        Footprints (two cols: footprint, healpixID).
+
+    """
+
+    df = pd.DataFrame()
+
+    import glob
+    fis = glob.glob('{}/*.hdf5'.format(dataDir))
+
+    for fi in fis:
+        dfa = pd.read_hdf(fi)
+        df = pd.concat((df, dfa))
+
+    return df
