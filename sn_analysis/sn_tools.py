4ac9452bd014b867e6f480ffdb11c87c6dc72ecb
pgris <gris@clermont.in2p3.fr>
2026-01-22 17:10:22 +0100

    update

--- sn_analysis/sn_tools.py
+++ sn_analysis/sn_tools.py
@@ -416,6 +416,8 @@ def complete_df(res, alpha=0.13, beta=3.1, Mb=-19.1):
     res['diff_mu'] = res['mu_exp']-res['mu']
     res['diff_mb'] = (res['mb']-res['mb_fit'])
 
+    res['chisq_red'] = res['chisq']/res['ndof']
+
     return res
 
 
