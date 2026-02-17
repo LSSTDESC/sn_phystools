99ecae475b1e6c20ecbec76e3e2c04243a492398
pgris <gris@clermont.in2p3.fr>
2026-02-17 16:14:18 +0100

    add few tools to fit histograms (like pulls)

---
+++ sn_analysis/sn_fit_tools.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 17 08:56:26 2026
+
+@author: philippe.gris@clermont.in2p3.fr
+"""
+
+from scipy.optimize import curve_fit
+import numpy as np
+
+def fit_hist(sel, pullvar,bins='auto',fit_with_errors=False):
+    """
+    Function to fit an histogram
+
+    Parameters
+    ----------
+    sel : array
+        Data to fit.
+    pullvar : str
+        variable to fit.
+    bins : array(float)
+        histogram bins.
+    fit_with_errors: bool, opt.
+        to fit the histogram with errors
+
+    Returns
+    -------
+    coeff : list(float)
+        fit parameter values (A, mu, sigma) (gauss).
+
+    """
+
+    hist, bins_h = np.histogram(sel[pullvar], bins=bins)
+    bin_centres = (bins_h[:-1] + bins_h[1:])/2
+    max_hist = np.max(hist)*np.sqrt(2.*np.pi)
+    p0 = [max_hist, 0., 1.]
+    nevts = np.sum(hist)
+    yerr = None
+    if fit_with_errors:
+        effi = hist/nevts
+        #yerr = 1./np.sqrt(nevts*effi*(1.-effi))
+        yerr = np.sqrt(hist*(1.-effi))/np.sqrt(nevts)
+        #empty bins: high errors
+        yerr[yerr == 0.] = 9999.
+
+    ndof = nevts-3
+    chi_square = 9999.
+    try:
+        
+        coeff, var_matrix = curve_fit(gauss, bin_centres, hist, p0=p0,
+                                      sigma=yerr)
+        err_coeff = np.sqrt(np.diag(var_matrix))
+        
+        exp = gauss(bin_centres,*coeff)
+        diff = hist-exp
+        vv = 1
+        if yerr is not None:
+            vv = yerr
+        chi_square = np.sum((diff/vv)**2)
+    
+    except Exception:
+        coeff = [-1, -1, -1]
+        err_coeff = [-1.,-1.,-1.]
+        chi_square=9999.
+        
+
+    return coeff,err_coeff,chi_square,ndof
+
+def gauss(x, *p):
+    """
+    gaussian function
+
+    Parameters
+    ----------
+    x : float
+        x values.
+    *p : list(float)
+        gaussian parameters.
+
+    Returns
+    -------
+    list(float)
+        function values.
+
+    """
+    A, mu, sigma = p
+    return A/(np.sqrt(2.*np.pi)*sigma)*np.exp(-(x-mu)**2/(2.*sigma**2))
