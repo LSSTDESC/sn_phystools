9869534d34c03a156dcbef22ec34670275b41056
pgris <gris@clermont.in2p3.fr>
2026-04-16 15:37:10 +0200

    update: add useful scripts

--- sn_analysis/sn_atmos_tools.py
+++ sn_analysis/sn_atmos_tools.py
@@ -184,6 +184,8 @@ def get_obs_values(grp,sigma):
     df = pd.DataFrame(res,columns=['sigma_obs_param'])
     
     df['sigma_atmos_param'] = sigmas
+    if 'config' in sigma.keys():
+        df['config'] = sigma['config']
     
     return df
 
@@ -215,3 +217,161 @@ def rename(dfa,atmos_params):
     df = df.rename(columns={'sigma_tot':'sigma_{}_tot'.format(obs_param)})
     
     return df
+
+def process_obs_data(df_zp,sigma,atmos_params,do_combi=False):
+    """
+    Data processing (per obs)
+
+    Parameters
+    ----------
+    df_zp : pandas df
+        Data to process.
+    sigma : dict(str,array(float))
+        List of sigma for atmos params (key).
+
+    Returns
+    -------
+    combis : pandas df
+        Processed data.
+
+    """
+    
+    interp_zp = get_atmos_data(df_zp,atmos_params=atmos_params)
+    cols = ['band', 'airmass', 'atmos_param', 'obs_param']
+    df_values = interp_zp.groupby(cols).apply(lambda x: get_obs_values(x,sigma),include_groups=False).reset_index()
+
+    print(df_values)
+
+    if do_combi:
+        combis = df_values.groupby(['band','airmass','obs_param']).apply(lambda x:make_combi(x),include_groups=False).reset_index()
+    else:
+        ccols = ['band','airmass','config']
+        combis = df_values.groupby(ccols).apply(lambda x:transform(x),include_groups=False).reset_index()
+        #return combis
+    
+    print(combis)
+
+    #estimate sigma_tot
+
+    combis['sigma_tot'] = 0
+
+    for atm in atmos_params:
+        combis['sigma_tot'] += combis['sigma_obs_param_{}'.format(atm)]**2
+    
+    combis['sigma_tot'] = np.sqrt(combis['sigma_tot'])
+
+    print(combis)
+
+    return combis
+
+def make_combi(grp,atmos_params=['airmass','ozone','aerosol','pwv']):
+    """
+    Function to make a combination of pandas sf
+
+    Parameters
+    ----------
+    grp : pandas df
+        Data to process.
+    atmos_params : list(str), optional
+        List of atmos params. The default is ['airmass','ozone','aerosol','pwv'].
+
+    Returns
+    -------
+    df_combi : pandas df
+        output data.
+
+    """
+    
+    cols = ['sigma_atmos_param','sigma_obs_param']
+    df_combi = pd.DataFrame()
+    for atm in atmos_params:
+        idx = grp['atmos_param'] == atm
+        sel = pd.DataFrame(grp[idx][cols])
+        sigma_atm = 'sigma_{}'.format(atm)
+        sigma_obs = 'sigma_obs_param_{}'.format(atm)
+        sel = sel.rename(columns={'sigma_atmos_param':sigma_atm,
+                                  'sigma_obs_param':sigma_obs})
+        if df_combi.empty:
+            df_combi = pd.DataFrame(sel)
+        else:
+            df_combi = df_combi.merge(sel, how='cross')
+        
+    return df_combi
+
+def merge_zp_wave(combi_zp,combi_wave,atmos_params):
+    """
+    Merge two df to form a single one
+
+    Parameters
+    ----------
+    combi_zp : pandas df
+        first df to merge.
+    combi_wave : pandas df
+        second df to merge.
+    atmos_params : list(str)
+        List of atmos params.
+
+    Returns
+    -------
+    combi_tot : pandas df
+        merged df.
+
+    """
+    
+    combi_zp = rename(combi_zp,atmos_params)
+    combi_wave = rename(combi_wave,atmos_params)
+    
+    combi_zp = combi_zp.drop(columns=['obs_param'])
+    combi_wave = combi_wave.drop(columns=['obs_param'])
+    
+    ccols = ['band','airmass']
+    
+    if 'config' in combi_zp.columns.to_list():
+        ccols += ['config']
+    for atm in atmos_params:
+        ccols += ['sigma_{}'.format(atm)]
+        
+    print('alalala',ccols)
+    combi_tot = combi_zp.merge(combi_wave,left_on=ccols,right_on=ccols)
+        
+    print(combi_tot.columns)    
+    
+    return combi_tot
+
+def transform(grp):
+    """
+    Function to transform a grp
+
+    Parameters
+    ----------
+    grp : pandas df
+        Data to transform.
+
+    Returns
+    -------
+    res : pandas df
+        Output data.
+
+    """
+    
+    obs_param = grp['obs_param'].unique()[0]
+    sigma_obs = grp['sigma_obs_param'].to_list()
+    
+    cola = grp['atmos_param'].to_list()
+    
+    cola = list(map(lambda x: 'sigma_obs_param_{}'.format(x),cola))
+    
+    sigma_atm = grp['sigma_atmos_param'].to_list()
+    
+    colb = grp['atmos_param'].to_list()
+    
+    colb = list(map(lambda x: 'sigma_{}'.format(x),colb))
+    
+    print(sigma_obs,cola)
+    
+    values = sigma_obs+sigma_atm
+    cols = cola+colb
+    
+    res = pd.DataFrame([values],columns=cols)
+    res['obs_param'] = obs_param
+    return res
